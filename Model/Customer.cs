e852811d4c197ccedc367fb3578d7a8f6c857607
Pankaj Tiwari <54172974+pankajkumar2701@users.noreply.github.com>
2024-02-02 11:03:47 +0530

    Upload Customer.cs

---
+++ Model/Customer.cs
@@ -0,0 +1,35 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Pankaj_code.Model
+{
+    /// <summary> 
+    /// Represents a customer entity with essential details
+    /// </summary>
+    public class Customer
+    {
+        /// <summary>
+        /// Primary key for the Customer 
+        /// </summary>
+        [Key]
+        public Guid CustomerId { get; set; }
+        /// <summary>
+        /// Name of the Customer 
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Address of the Customer 
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// CountryName of the Customer 
+        /// </summary>
+        public string CountryName { get; set; }
+        /// <summary>
+        /// Collection navigation property representing associated 
+        /// </summary>
+        public ICollection<Order> Orders { get; set; }
+    }
+}
